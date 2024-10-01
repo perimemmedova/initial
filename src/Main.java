9d14bf353cc6d21cc440d43a3f4c8307c2af0887
perimemmedova <perismemmedova354@gmail.com>
2024-10-01 20:43:30 +0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int [] numbers = {1,2,3,4,5,6,7,8,9};
+
+        }
+    }
